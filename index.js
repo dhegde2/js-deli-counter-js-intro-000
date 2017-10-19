6aa8b1f85b49c229d19484051310f13ba7060418
Dhruv Hegde <desidhruv@gmail.com>
2017-10-19 23:47:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 function takeANumber(array, name){
-let n = array.indexOf(name) + 1
-return 'Welcome ' + name + '. You are number ' + n+1 + ' in line.'; 
+let n = (array.indexOf(name)) + 1
+return 'Welcome, ' + name + '. You are number ' +  + ' in line.'; 
 }
