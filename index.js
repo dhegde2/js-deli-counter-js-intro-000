7e6961dd843f2ae34834f465a10afee898f8715c
Dhruv Hegde <desidhruv@gmail.com>
2017-10-19 23:59:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,11 @@ let n = katzDeliLine.indexOf("name");
 let a = n + 1
 return 'Welcome, ' + name + '. You are number ' + a + ' in line.';
 }
+function nowServing(katzDeliLine){ 
+if(katzDeliLine.length==0){ 
+return 'There is nobody waiting to be served'; 
+} else { 
+  return 'Currently serving ' + katzDeliLine[0]; 
+  delete katzDeliLine[0]; 
+}
+}
