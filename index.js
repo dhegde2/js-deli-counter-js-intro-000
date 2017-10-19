8f5aec65eff5e845078c47c05a487bd039f707a8
Dhruv Hegde <desidhruv@gmail.com>
2017-10-19 23:51:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-let ketzDeli = []; 
+let ketzDeli = [];
 function takeANumber(array,name){
-let n = array.indexOf("name"); 
+let n = array.indexOf("name");
 let a = n + 1
 return 'Welcome, ' + name + '. You are number ' + a + ' in line.';
 }
