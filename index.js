78ab2758ffc5299a85e8d189b73b092e3d060b98
Dhruv Hegde <desidhruv@gmail.com>
2017-10-20 00:02:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 let ketzDeli = [];
-function takeANumber(katzDeliLine,name){
-let n = katzDeliLine.indexOf(name);
+function takeANumber(array,name){
+let n = array.indexOf(name);
 let a = n + 1
 return 'Welcome, ' + name + '. You are number ' + a + ' in line.';
 }
-function nowServing(katzDeliLine){
+function nowServing(array){
 if(katzDeliLine.length==0){
 return 'There is nobody waiting to be served!';
 } else {
-  return 'Currently serving ' + katzDeliLine[0] + '.';
-  delete katzDeliLine[0];
+  return 'Currently serving ' + array[0] + '.';
+  delete array[0];
 }
 }
